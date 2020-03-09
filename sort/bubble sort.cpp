d51822d234065b6aac56beafafbe81b842a33509
mu001999 <mu001999@outlook.com>
2020-03-09 22:25:03 +0800

    fix bug when the size of input is 1

--- sort/bubble sort.cpp
+++ sort/bubble sort.cpp
@@ -4,30 +4,16 @@
 
 using namespace std;
 
-template<int val, int ...vals> struct Vals
-{
-    constexpr static int fval = val;
-    constexpr static size_t size = sizeof...(vals) + 1;
-    using TailsType = Vals<vals...>;
-};
+template<int ...vals> struct Vals {};
 
-template<int val> struct Vals <val>
+template<int i, typename T> struct ValueAt;
+template<int i, int val0, int ...vals> struct ValueAt<i, Vals<val0, vals...>>
 {
-    constexpr static int fval = val;
-    constexpr static size_t size = 1;
-    using TailsType = void;
+    constexpr static int val = ValueAt<i - 1, Vals<vals...>>::val;
 };
-
-template<int i, typename T> struct ValueAt
+template<int val0, int ...vals> struct ValueAt<0, Vals<val0, vals...>>
 {
-    using type = typename ValueAt<i - 1, typename T::TailsType>::type;
-    constexpr static int val = type::fval;
-};
-
-template<int  ...vals> struct ValueAt<0, Vals<vals...>>
-{
-    using type = Vals<vals...>;
-    constexpr static int val = type::fval;
+    constexpr static int val = val0;
 };
 
 template<typename T1, typename T2> struct Combine;
@@ -36,23 +22,15 @@ template<int ...vals1, int ...vals2> struct Combine<Vals<vals1...>, Vals<vals2..
     using type = Vals<vals1..., vals2...>;
 };
 
-template<int i, typename T> struct SwapValue
+template<int i, typename T> struct SwapValue;
+template<int i, int val, int ...vals> struct SwapValue<i, Vals<val, vals...>>
 {
-    using type = typename Combine<
-        Vals<T::fval>,
-        typename SwapValue<i - 1, typename T::TailsType>::type>::type;
+    using type = typename Combine<Vals<val>, typename SwapValue<i - 1, Vals<vals...>>::type>::type;
 };
-
-template<int ...vals> struct SwapValue<0, Vals<vals...>>
+template<int val1, int val2, int ...vals> struct SwapValue<0, Vals<val1, val2, vals...>>
 {
-    using V = Vals<vals...>;
-    using type = typename Combine<
-        Vals<ValueAt<1, V>::val>,
-        typename Combine<Vals<
-            ValueAt<0, V>::val>,
-            typename Vals<vals...>::TailsType::TailsType>::type>::type;
+    using type = typename Combine<Vals<val2>, typename Combine<Vals<val1>, Vals<vals...>>::type>::type;
 };
-
 template<int val1, int val2> struct SwapValue<0, Vals<val1, val2>>
 {
     using type = Vals<val2, val1>;
@@ -64,12 +42,14 @@ template<int i, int j, typename T> struct BubbleSort
         BubbleSort<i, j + 1, typename SwapValue<j - 1, T>::type>,
         BubbleSort<i, j + 1, T>>::type;
 };
-
+template<int val> struct BubbleSort<1, 1, Vals<val>>
+{
+    using type = Vals<val>;
+};
 template<int j, int ...vals> struct BubbleSort<sizeof...(vals), j, Vals<vals...>>
 {
     using type = Vals<vals...>;
 };
-
 template<int i, int ...vals> struct BubbleSort<i, sizeof...(vals), Vals<vals...>>
 {
     using type = typename BubbleSort<i + 1, 1, Vals<vals...>>::type;
@@ -89,6 +69,8 @@ template<int ...vals> void print(Vals<vals...>)
 
 int main()
 {
+    print(Sort<Vals<1>>::type());
+    print(Sort<Vals<2,1>>::type());
     print(Sort<Vals<3,5,2,8,7,1,6,9,4>>::type());
     return 0;
 }
