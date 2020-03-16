6be7e12482890d7d5bca3d123f4d832dbf6b1d79
mu001999 <mu001999@outlook.com>
2020-03-16 21:50:02 +0800

    update

---
+++ lambda/lambda_type.hpp
@@ -0,0 +1,27 @@
+#include <typeinfo>
+#include <functional>
+#include <type_traits>
+
+template<typename T>
+struct lambda_traits : lambda_traits<decltype(&T::operator())> {};
+
+template<typename ClassType, typename R, typename ...Args>
+struct lambda_traits<R(ClassType::*)(Args...) const> {
+    using result_type = R;
+    using args_type = std::tuple<Args...>;
+    template<size_t index>
+    using arg_type_at = std::tuple_element_t<index, args_type>;
+    static constexpr size_t arity = sizeof...(Args);
+};
+
+/* Test:
+template<typename Func>
+void test(Func &&f) {
+    std::cout << typeid(typename lambda_traits<Func>::result_type).name() << std::endl;
+}
+
+int main()
+{
+    test([]() -> double { return 1.0; });
+}
+*/
